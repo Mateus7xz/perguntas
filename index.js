c4db86e6f20b103a328ea5364f438ea4bfb74e45
Mateus <mateusmverde@gmail.com>
2026-08-18 21:08:03 -0300

    configurado botões e finalizando páginas de resposta

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const Pergunta = require("./database/pergunta");
+const Resposta = require("./database/Resposta");
 
 //Database
 connection.authenticate().then(() => {
@@ -47,8 +48,20 @@ app.post("/salvarpergunta", (req,res) => {
     });
 });
 
+// tela da pergunta.ejs
 app.get("/pergunta/:id", (req,res) => {
     var id = req.params.id;
+    Pergunta.findOne({
+        where:{id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined){ // Achou a pergunta
+            res.render("pergunta",{
+                pergunta: pergunta
+            });
+            }else{ //Não encotrou
+                res.redirect("/");           
+        };
+    });
 });
 
 //servidor
